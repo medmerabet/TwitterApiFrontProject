c4e0bf1b54757097da9f4b7bf69ba4d177091b25
mehdi merabet <mehdimerabet@MacBook-Pro-de-mehdi.local>
2020-06-28 21:27:07 +0200

    authentifation with token using login register

--- src/nimn-front-app/src/app/app.module.ts
+++ src/nimn-front-app/src/app/app.module.ts
@@ -1,43 +1,41 @@
-
-import { NavbarComponent } from './navbar/navbar.component';
-import { RegisterComponent } from './register/register.component';
-
+// built-in
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule} from '@angular/forms'; 
-import { AppRoutingModule } from './app-routing.module';
-import {RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+// components
 import { AppComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCardModule,MatToolbarModule, MatInputModule} from '@angular/material';
-import {ApiService} from "./api.service";
-
+import { UserComponent } from './user/user.component';
+import { SignUpComponent } from './user/sign-up/sign-up.component';
+//routes
+import { appRoutes } from './routes';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { UserService } from './shared/user.service';
+//other
+import { AuthGuard } from './auth/auth.guard';
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    RegisterComponent
+    UserComponent,
+    SignUpComponent,
+    UserProfileComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
     FormsModule,
-    RouterModule,
-    MatButtonModule, 
-    MatCardModule,
-    MatToolbarModule,
-    MatInputModule,
-    RouterModule.forRoot([
-      {path :"register", component: RegisterComponent}
-    ])
-
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
-  providers: [ApiService],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: AuthInterceptor,
+    multi: true
+  },AuthGuard,UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
